1e899ef7d48390d054e9e9fe0f6d39c3b6cf4ce8
Marcelino Pérez <32309834+mperez01@users.noreply.github.com>
2018-03-14 17:13:57 +0100

    Algoritmo A*
    Implementación básica del algoritmo sin obstáculos.
     A implementar:
    - Generar camino con obstáculos.
    - Añadir extensiones

--- aStar/js/index.js
+++ aStar/js/index.js
@@ -4,14 +4,30 @@
  */
 var startCoord;
 var endCoord;
+var startNodeMatrix = {
+    x: 0,
+    y: 0
+};
+var endNodeMatrix = {
+    x: 0,
+    y: 0
+}
 /** Style, exceeds width screen. If two menu, recommended max 40, if only one menu, 48 */
 var maxCol = 48;
+var maxRow = 100;
 var IsClickDown;
+
+
+/**
+ *  PDF in html5
+ *  <iframe src="http://www.aragonemprendedor.com/archivos/descargas/tutorial_googledocs.pdf" 
+        style="width:600px; height:500px;" frameborder="0"></iframe>
+ */
 
 $(() => {
 
     defaultBoard();
-    
+
     $('#apply').on("click", createBoard);
 
     $('table').on("click", "td", clickBoard);
@@ -22,7 +38,7 @@ $(() => {
         drawObstacle(event);
     });
 
-    /** Mouse up in html better */
+    /** Mouse up in html better, but this isn't mobile friend */
     $("html").mouseup(() => {
         if (IsClickDown) {
             IsClickDown = false;
@@ -81,6 +97,55 @@ function aStar() {
         mssgAlertRemove();
         var matrix = getMatrix();
         console.log(matrix);
+        // neighbour
+        var ady = neighbour(matrix, startNodeMatrix);
+        // if doesn't exist neighbour
+        if (ady.length <= 0) {
+            alert("¡No hay adyacentes!");
+        } else {
+            //comprobamos el que menos cueste
+            var smaller = -1;
+            var smallerValue = -1;
+            while (smallerValue > Math.sqrt(2) || smallerValue === -1) {
+                for (var i = 0; i < ady.length; i++) {
+                    //Auxiliary to call Euclidean once
+                    var aux = euclidean(ady[i], endNodeMatrix);
+                    /**
+                     * Adding square 2 or 1 if the adyacent node is diagonal or not
+                     */
+                    //console.log("(" + ady[i].x + "," + ady[i].y + ")" + "Aux sin siguiente suma = " + aux);
+                    if (ady[i].isDiagonal) {
+                        aux += Math.sqrt(2);
+                    } else {
+                        aux += 1;
+                    }
+                    //console.log("(" + ady[i].x + "," + ady[i].y + ")" + "Aux con siguiente suma = " + aux);
+                    // **************************
+
+                    if (smaller === -1) {
+                        smaller = ady[i];
+                        smallerValue = aux;
+                    } else if (Number(smallerValue) > Number(aux)) {
+                        smaller = ady[i];
+                        smallerValue = aux;
+                    }
+                    //console.log("(" + ady[i].x + "," + ady[i].y + ")" + " calculo=" + aux);
+                }
+                console.log(ady);
+
+                //COLOREAMOS EL NODO, sólo si no es el último (nodo destino):
+                if (smallerValue > Math.sqrt(2)) {
+                    var myClass = ".r" + smaller.x + ".c" + smaller.y;
+                    $(String(myClass)).addClass("road");
+                }
+                //****************** */
+
+                //Nuevo nodo a calcular
+                ady = neighbour(matrix, smaller);
+                //console.log("Adyacente de menor coste = (" + smaller.x + "," + smaller.y + ")");
+            }
+        }
+
     } else {
         alert("Falta inicio o destino");
     }
@@ -98,13 +163,36 @@ function getMatrix() {
         matrix[i] = [];
         for (var j = 0; j < numColumns; j++) {
             if ($($($("tr")[i]).children("td")[j]).hasClass("start")) {
-                matrix[i][j] = "start"
+                /**
+                 * x = filas, y = columnas
+                 */
+                matrix[i][j] = {
+                    status: "start",
+                    x: i,
+                    y: j
+                }
+                startNodeMatrix.x = i;
+                startNodeMatrix.y = j;
             } else if ($($($("tr")[i]).children("td")[j]).hasClass("end")) {
-                matrix[i][j] = "end"
+                matrix[i][j] = {
+                    status: "end",
+                    x: i,
+                    y: j
+                }
+                endNodeMatrix.x = i;
+                endNodeMatrix.y = j;
             } else if ($($($("tr")[i]).children("td")[j]).hasClass("obstacle")) {
-                matrix[i][j] = "obstacle"
+                matrix[i][j] = {
+                    status: "obstacle",
+                    x: i,
+                    y: j
+                }
             } else {
-                matrix[i][j] = "free"
+                matrix[i][j] = {
+                    status: "free",
+                    x: i,
+                    y: j
+                }
             }
         }
     }
@@ -209,6 +297,8 @@ function createBoard(event) {
             alert("¡Error al introducir los datos!")
         } else if (Number(columns) > maxCol) {
             alert("Máximo " + maxCol + " columnas")
+        } else if (Number(rows) > maxRow) {
+            alert("Máximo " + maxRow + " filas")
         } else {
             /**Put empty the field value */
             $("#row").val("");
@@ -229,6 +319,13 @@ function board(rows, columns) {
     IsClickDown = false;
     startCoord = null;
     endCoord = null;
+
+    startNodeMatrix.x = null;
+    startNodeMatrix.y = null;
+
+    endNodeMatrix.x = null;
+    endNodeMatrix.y = null;
+
 
     $(".infoDim").text("Tablero " + rows + " x " + columns);
     var table = $("table");
@@ -264,4 +361,105 @@ function cleanAll() {
     mssgAlertRemove();
     $("#row").val("");
     $("#col").val("");
+}
+
+/**
+ * Miramos la distancia euclidea de los ayaccentes del inicio respecto al nodo final
+ * distancia de inicio a adyacente + euclidea
+ * 
+ * Nos quedamos con el de menor coste de todos los adyacentes. Los obstáculos no son adyacentes y se ignoran
+ */
+
+/**
+ * Matrix, coord 0,0 it's in the top left
+ * 
+ * @param {*} matrix matrix nodes coord and status
+ */
+function neighbour(matrix, start) {
+    var ady = [];
+
+    var x = start.x;
+    var y = start.y;
+
+    // West
+    if (matrix[x - 1] && matrix[x - 1][y]) {
+        if (matrix[x - 1][y].status != "obstacle") {
+            ady.push(matrix[x - 1][y]);
+            ady[ady.length - 1].isDiagonal = false;
+        }
+    }
+
+    // East
+    if (matrix[x + 1] && matrix[x + 1][y]) {
+        if (matrix[x + 1][y].status != "obstacle") {
+            ady.push(matrix[x + 1][y]);
+            ady[ady.length - 1].isDiagonal = false;
+        }
+    }
+
+    // North
+    if (matrix[x] && matrix[x][y - 1]) {
+        if (matrix[x][y - 1].status != "obstacle") {
+            ady.push(matrix[x][y - 1]);
+            ady[ady.length - 1].isDiagonal = false;
+        }
+    }
+
+    // South
+    if (matrix[x] && matrix[x][y + 1]) {
+        if (matrix[x][y + 1].status != "obstacle") {
+            ady.push(matrix[x][y + 1]);
+            ady[ady.length - 1].isDiagonal = false;
+        }
+    }
+
+    // -------- DIAGONALS -------------
+
+    // Northwest
+    if (matrix[x - 1] && matrix[x - 1][y - 1]) {
+        if (matrix[x - 1][y - 1].status != "obstacle") {
+            ady.push(matrix[x - 1][y - 1]);
+            ady[ady.length - 1].isDiagonal = true;
+        }
+    }
+
+    // Northheast
+    if (matrix[x + 1] && matrix[x + 1][y - 1]) {
+        if (matrix[x + 1][y - 1].status != "obstacle") {
+            ady.push(matrix[x + 1][y - 1]);
+            ady[ady.length - 1].isDiagonal = true;
+        }
+    }
+
+    // Southwest
+    if (matrix[x - 1] && matrix[x - 1][y + 1]) {
+        if (matrix[x - 1][y + 1].status != "obstacle") {
+            ady.push(matrix[x - 1][y + 1]);
+            ady[ady.length - 1].isDiagonal = true;
+        }
+    }
+
+    // Southeast
+    if (matrix[x + 1] && matrix[x + 1][y + 1]) {
+        if (matrix[x + 1][y + 1].status != "obstacle") {
+            ady.push(matrix[x + 1][y + 1]);
+            ady[ady.length - 1].isDiagonal = true;
+        }
+    }
+
+    // --------------------------------
+
+    return ady;
+}
+
+/**
+ * Math.Abs(nodoActual.X – nodoFinal.X) + Math.Abs(nodoActual.Y – nodoFinal.Y)
+ * 
+ * @param {*} pos0 Nodoactual 
+ * @param {*} endNodeMatrix nodoFinal
+ */
+function euclidean(pos0, endNodeMatrix) {
+    var d1 = Math.pow((endNodeMatrix.x - pos0.x), 2);
+    var d2 = Math.pow((endNodeMatrix.y - pos0.y), 2);
+    return Math.sqrt(d1 + d2);
 }
