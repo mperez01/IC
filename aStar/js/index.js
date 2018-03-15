356f74a6093b463e55df10cdebec809d9db34b21
Marcelino Pérez <32309834+mperez01@users.noreply.github.com>
2018-03-15 22:03:06 +0100

    Algoritmo funcionando
    Queda añadir extensiones

--- aStar/js/index.js
+++ aStar/js/index.js
@@ -6,8 +6,10 @@ var startCoord;
 var endCoord;
 var startNodeMatrix = {
     x: 0,
-    y: 0
+    y: 0,
+    g: 0
 };
+
 var endNodeMatrix = {
     x: 0,
     y: 0
@@ -91,63 +93,93 @@ $(() => {
 })
 
 function aStar() {
+    /** Remove the last road */
+    $("*").removeClass("road");
     /** First, check if exists start and end coordenate */
     if (startCoord !== null && endCoord !== null) {
         //Getting the board matrix
         mssgAlertRemove();
         var matrix = getMatrix();
-        console.log(matrix);
         // neighbour
-        var ady = neighbour(matrix, startNodeMatrix);
-        // if doesn't exist neighbour
-        if (ady.length <= 0) {
-            alert("¡No hay adyacentes!");
-        } else {
-            //comprobamos el que menos cueste
-            var smaller = -1;
-            var smallerValue = -1;
-            while (smallerValue > Math.sqrt(2) || smallerValue === -1) {
-                for (var i = 0; i < ady.length; i++) {
-                    //Auxiliary to call Euclidean once
-                    var aux = euclidean(ady[i], endNodeMatrix);
+        var openHeap = [];
+        var closeHeap = [];
+        //First open node is start node
+        openHeap.push(startNodeMatrix);
+        var find = false;
+        var error = false;
+        do {
+            if (openHeap.length === 0) {
+                error = true;
+                alert("No hay solución");
+            } else {
+
+                var currentNode = openHeap.pop();
+                closeHeap.push(currentNode);
+
+                if (currentNode.status === "end") {
+                    find = true;
+                    console.log("Encotrado")
+
                     /**
-                     * Adding square 2 or 1 if the adyacent node is diagonal or not
-                     */
-                    //console.log("(" + ady[i].x + "," + ady[i].y + ")" + "Aux sin siguiente suma = " + aux);
-                    if (ady[i].isDiagonal) {
-                        aux += Math.sqrt(2);
-                    } else {
-                        aux += 1;
+                    * Get all "parents" from curretnNode (end)
+                    */
+                    var end = false;
+                    var search = currentNode;
+                    while (!end) {
+                        if (search.parent === undefined) {
+                            end = true;
+                        } else {
+                            var myClass = ".r" + search.parent.x + ".c" + search.parent.y;
+                            $(String(myClass)).addClass("road");
+                            search = search.parent;
+                        }
                     }
-                    //console.log("(" + ady[i].x + "," + ady[i].y + ")" + "Aux con siguiente suma = " + aux);
-                    // **************************
 
-                    if (smaller === -1) {
-                        smaller = ady[i];
-                        smallerValue = aux;
-                    } else if (Number(smallerValue) > Number(aux)) {
-                        smaller = ady[i];
-                        smallerValue = aux;
+                } else {
+                    var ady = neighbour(matrix, currentNode);
+                    for (var i = 0; i < ady.length; i++) {
+                        var neig = ady[i];
+                        //var visited = neig.visited;
+                        if (!openHeap.includes(neig) && !closeHeap.includes(neig) && neig.status != "start") {
+                            neig.parent = currentNode;
+                            //Ordenar lista de menor a mayor
+                            neig.h = euclidean(neig, endNodeMatrix);
+                            neig.g = Number(calulateG(neig, neig.parent.g));
+                            // f(x) = g(x) + h(x)
+                            neig.f = neig.g + neig.h;
+                            openHeap.push(neig);
+                        } else if (openHeap.includes(neig) && !closeHeap.includes(neig) && neig.status != "start") {
+                            var newG = calulateG(neig, neig.parent.g);
+                            var newH = euclidean(neig, endNodeMatrix);
+                            var newF = newG + newH;
+                            if ( newF < neig.f) {
+                                neig.f = newF;
+                                neig.h = newH;
+                                neig.g = newG;
+                                neig.parent = currentNode;
+                            }
+                        }
                     }
-                    //console.log("(" + ady[i].x + "," + ady[i].y + ")" + " calculo=" + aux);
-                }
-                console.log(ady);
 
-                //COLOREAMOS EL NODO, sólo si no es el último (nodo destino):
-                if (smallerValue > Math.sqrt(2)) {
-                    var myClass = ".r" + smaller.x + ".c" + smaller.y;
-                    $(String(myClass)).addClass("road");
+                    openHeap.sort(function (obj1, obj2) {
+                        // first f(x) less than the previous
+                        return obj1.g - obj2.g;
+                    });
+                    openHeap.reverse();
                 }
-                //****************** */
-
-                //Nuevo nodo a calcular
-                ady = neighbour(matrix, smaller);
-                //console.log("Adyacente de menor coste = (" + smaller.x + "," + smaller.y + ")");
             }
-        }
+        } while (!find && !error);
 
     } else {
         alert("Falta inicio o destino");
+    }
+}
+
+function calulateG(node, lastG) {
+    if (node.isDiagonal) {
+        return lastG + Math.sqrt(2);
+    } else {
+        return lastG + 1;
     }
 }
 
@@ -164,12 +196,17 @@ function getMatrix() {
         for (var j = 0; j < numColumns; j++) {
             if ($($($("tr")[i]).children("td")[j]).hasClass("start")) {
                 /**
-                 * x = filas, y = columnas
+                 * x = rows, y = columns
                  */
+
                 matrix[i][j] = {
                     status: "start",
                     x: i,
-                    y: j
+                    y: j,
+                    parent: null,
+                    f: 0,
+                    g: 0,
+                    h: 0
                 }
                 startNodeMatrix.x = i;
                 startNodeMatrix.y = j;
@@ -177,7 +214,11 @@ function getMatrix() {
                 matrix[i][j] = {
                     status: "end",
                     x: i,
-                    y: j
+                    y: j,
+                    parent: null,
+                    f: 0,
+                    g: 0,
+                    h: 0
                 }
                 endNodeMatrix.x = i;
                 endNodeMatrix.y = j;
@@ -185,13 +226,21 @@ function getMatrix() {
                 matrix[i][j] = {
                     status: "obstacle",
                     x: i,
-                    y: j
+                    y: j,
+                    parent: null,
+                    f: 0,
+                    g: 0,
+                    h: 0
                 }
             } else {
                 matrix[i][j] = {
                     status: "free",
                     x: i,
-                    y: j
+                    y: j,
+                    parent: null,
+                    f: 0,
+                    g: 0,
+                    h: 0
                 }
             }
         }
@@ -214,6 +263,7 @@ function clickBoard(event) {
                 startCoord = selected.attr("class");
                 $(".coordS").text(" Coordenada de inicio " + c.split(' ')[0] + " " + c.split(' ')[1]);
                 selected.toggleClass("start");
+                $("*").removeClass("road");
             }
         } else {
             if (selected.hasClass("end")) {
@@ -224,6 +274,7 @@ function clickBoard(event) {
                 selected.removeClass("obstacle");
                 $(".coordS").text(" Coordenada de inicio " + c.split(' ')[0] + " " + c.split(' ')[1]);
                 selected.toggleClass("start");
+                $("*").removeClass("road");
             }
         }
     }
@@ -235,6 +286,7 @@ function clickBoard(event) {
                 endCoord = selected.attr("class");
                 $(".coordE").text(" Coordenada de destino " + c.split(' ')[0] + " " + c.split(' ')[1]);
                 selected.toggleClass("end");
+                $("*").removeClass("road");
             }
         } else {
             if (selected.hasClass("start")) {
@@ -245,6 +297,7 @@ function clickBoard(event) {
                 selected.removeClass("obstacle");
                 $(".coordE").text(" Coordenada de destino " + c.split(' ')[0] + " " + c.split(' ')[1]);
                 selected.toggleClass("end");
+                $("*").removeClass("road");
             }
         }
     }
@@ -361,6 +414,7 @@ function cleanAll() {
     mssgAlertRemove();
     $("#row").val("");
     $("#col").val("");
+    $("*").removeClass("road");
 }
 
 /**
@@ -385,7 +439,6 @@ function neighbour(matrix, start) {
     if (matrix[x - 1] && matrix[x - 1][y]) {
         if (matrix[x - 1][y].status != "obstacle") {
             ady.push(matrix[x - 1][y]);
-            ady[ady.length - 1].isDiagonal = false;
         }
     }
 
@@ -393,7 +446,6 @@ function neighbour(matrix, start) {
     if (matrix[x + 1] && matrix[x + 1][y]) {
         if (matrix[x + 1][y].status != "obstacle") {
             ady.push(matrix[x + 1][y]);
-            ady[ady.length - 1].isDiagonal = false;
         }
     }
 
@@ -401,7 +453,6 @@ function neighbour(matrix, start) {
     if (matrix[x] && matrix[x][y - 1]) {
         if (matrix[x][y - 1].status != "obstacle") {
             ady.push(matrix[x][y - 1]);
-            ady[ady.length - 1].isDiagonal = false;
         }
     }
 
@@ -409,7 +460,6 @@ function neighbour(matrix, start) {
     if (matrix[x] && matrix[x][y + 1]) {
         if (matrix[x][y + 1].status != "obstacle") {
             ady.push(matrix[x][y + 1]);
-            ady[ady.length - 1].isDiagonal = false;
         }
     }
 
